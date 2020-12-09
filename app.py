ca63693fc8d836ff7a5b434dfe03c8411ef057c3
XPerianer <dominik.meier@student.hpi.de>
2020-12-09 17:11:53 +0100

    broadcast of pytest to web

--- app.py
+++ app.py
@@ -1,27 +1,42 @@
 from flask import Flask
 from flask import jsonify
+from flask_cors import CORS
+from flask_socketio import SocketIO
+from flask_socketio import join_room
+
 import subprocess
 import multitasking
+import json
 
 app = Flask(__name__)
+CORS(app)
+socketio = SocketIO(app, cors_allowed_origins='*')
 
 test_data = {}
 
-@app.route('/')
+@app.route('/data')
 def test_data():
-    return jsonify(d)
+    data = {}
+    with open('test_visualization_data.json') as json_file:
+        data = json.load(json_file)
+    print("Serve data request")
+    return jsonify(data)
+
+@socketio.on('message')
+def handle_message(message):
+    print('received message: ' + message)
+
+@socketio.on('join')
+def handle_my_custom_event(str):
+    print('received join: ' + str)
+    join_room(str)
+    print('Jointed room ' + str)
 
 
-@multitasking.task
-def update_test_data():
-    try:
-        while True:
-            print("Starting Tests")
-            subprocess.call('cd /home/dominik/Studium/9_Semester/PLCTE/flask/ && . venv/bin/activate ' \
-                    '&& pytest -rN --json=report.json', shell=True)
-    except KeyboardInterrupt:
-        print('interrupted!')
+@socketio.on('testreport')
+def handle_my_custom_event(json):
+    print('received test report: ' + str(json))
+    socketio.emit('testreport', json, room='web')
 
 if __name__ == '__main__':
-    update_test_data()
-    app.run(debug=True)
+    socketio.run(app, port=9001, debug=True)
